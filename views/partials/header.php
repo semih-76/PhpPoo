5200875882d2ed5cebdfc6d5315e0c51a90ce46c
semih-76 <semihsisman7@gmail.com>
2026-05-20 09:45:05 +0200

    config header

---
+++ views/partials/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= h($title ?? 'Médiathèque') ?></title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <a href="/index.php">Médiathèque interne</a>
+        <a href="/create.php">Ajouter une ressource</a>
+    </nav>
+</header>
+<main>
